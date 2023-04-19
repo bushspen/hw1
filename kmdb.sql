630ba21160454f97073dd2f220d7141e25d83152
Spencer Bush <spencer.bush@kellogg.northwestern.edu>
2023-04-19 12:22:25 +0000

    created domain, inserted data, & created report

--- kmdb.sql
+++ kmdb.sql
@@ -106,12 +106,317 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS roles;
+
 -- Create new tables, according to your domain model
 -- TODO!
+
+CREATE TABLE films (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  film_title TEXT,
+  year_released TEXT,
+  mpaa_rating TEXT,
+  studio_name TEXT
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  actor_name TEXT
+);
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  role_name TEXT,
+  film_id INTEGER,
+  actor_id INTEGER
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+INSERT INTO films (
+  film_title,
+  year_released,
+  mpaa_rating,
+  studio_name
+)
+VALUES (
+  "Batman Begins",
+  "2005",
+  "PG-13",
+  "Warner Bros."
+);
+
+INSERT INTO films (
+  film_title,
+  year_released,
+  mpaa_rating,
+  studio_name
+)
+VALUES (
+  "The Dark Knight",
+  "2008",
+  "PG-13",
+  "Warner Bros."
+);
+
+INSERT INTO films (
+  film_title,
+  year_released,
+  mpaa_rating,
+  studio_name
+)
+VALUES (
+  "The Dark Knight Rises",
+  "2012",
+  "PG-13",
+  "Warner Bros."
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Christian Bale"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Michael Caine"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Liam Neeson"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Katie Holmes"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Gary Oldman"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Heath Ledger"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Aaron Eckhart"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Maggie Gyllenhaal"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Tom Hardy"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Joseph Gordon-Levitt"
+);
+
+INSERT INTO actors (
+  actor_name
+)
+VALUES (
+  "Anne Hathaway"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Bruce Wayne",
+  "1",
+  "1"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Alfred",
+  "1",
+  "2"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Ra's Al Ghul",
+  "1",
+  "3"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Rachel Dawes",
+  "1",
+  "4"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Comissioner Gordon",
+  "1",
+  "5"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Bruce Wayne",
+  "2",
+  "1"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Joker",
+  "2",
+  "6"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Harvey Dent",
+  "2",
+  "7"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Alfred",
+  "2",
+  "2"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Rachel Dawes",
+  "2",
+  "8"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Bruce Wayne",
+  "3",
+  "1"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Commissioner Gordon",
+  "3",
+  "5"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Bane",
+  "3",
+  "9"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "John Blake",
+  "3",
+  "10"
+);
+
+INSERT INTO roles (
+  role_name,
+  film_id,
+  actor_id
+)
+VALUES (
+  "Selina Kyle",
+  "3",
+  "11"
+);
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -121,12 +426,29 @@
 -- The SQL statement for the movies output
 -- TODO!
 
+SELECT 
+  film_title,
+  year_released,
+  mpaa_rating,
+  studio_name
+FROM 
+  films;
+
 -- Prints a header for the cast output
 .print ""
 .print "Top Cast"
 .print "========"
 .print ""
 
-
 -- The SQL statement for the cast output
 -- TODO!
+
+SELECT 
+  films.film_title,
+  actors.actor_name,
+  roles.role_name
+FROM 
+  roles 
+    JOIN films ON roles.film_id = films.id
+    JOIN actors ON roles.actor_id = actors.id
+;
